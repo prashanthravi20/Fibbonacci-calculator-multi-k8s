0aa973a45b26381cef8c4c7afd6495c551a2ca51
Prashanth Ravi <prashanth20@gmail.com>
2019-08-11 12:15:53 -0500

    Initial commit

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -64,10 +64,10 @@ class Fib extends Component {
           <button>Submit</button>
         </form>
 
-        <h3>Indexes I have seen:</h3>
+        <h3>Indexes I have seen (Origin Postgres):</h3>
         {this.renderSeenIndexes()}
 
-        <h3>Calculated Values:</h3>
+        <h3>Calculated Values (Origin Redis/ Worker):</h3>
         {this.renderValues()}
       </div>
     );
